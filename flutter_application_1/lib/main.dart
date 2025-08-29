12509138b3f9328fd415c27dd2b71a4d0db0b498
Bea <biabatista1219@gmail.com>
2025-08-29 09:21:48 -0300

    mudei tudo

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,19 +1,23 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_application_1/myhomepage.dart';
+import 'package:flutter/material.dart';
+
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Tarefas",
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: MyHomePage()
     );
   }
 }
--- flutter_application_1/lib/myhomepage.dart
+++ flutter_application_1/lib/myhomepage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+ 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
  
@@ -8,146 +8,250 @@ class MyHomePage extends StatefulWidget {
 }
  
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Map<String,String>> _tarefas = [];
-  
-  void _adicionarTarefas(String url, String descricao, BuildContext context){
+  final List<Map<String, String>> _tarefas = [];
+ 
+  //criando metodo para salvar tarefas
+  void _adicionarTarefas(String url, String descricao, BuildContext context) {
+    //metodos set
     setState(() {
-      _tarefas.add({
-          'url': url,
-          'descricao': descricao
-      });
+      _tarefas.add({'url': url, 'descricao': descricao});
     });
   }
-
-
+ 
+  //Criando um modal
+  //metodo void modal cadastrar
   void _showForm(BuildContext context) {
+    //criando as variaveis de controller
     final TextEditingController imageController = TextEditingController();
     final TextEditingController descricaoController = TextEditingController();
     showDialog(
-      context: context, 
-      builder: (BuildContext context){
+      context: context,
+      builder: (BuildContext context) {
         return Dialog(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10),
-        ),
-        child:SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          ),
+          child: SingleChildScrollView(
+            //Retangulo vai se adaptar ao conteudo aos componentes
+            child: Container(
+              //Criando o container
+              //margem interna
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  Text("Cadastrar Atividade",
-                  style: TextStyle(
-                    fontFamily:"Verdana",
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Cadastrar Atividade",
+                        style: TextStyle(
+                          fontFamily: "Verdana",
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          //ação do icone botão fechar
+ 
+                          Navigator.of(context).pop();
+                        },
+                        icon: Icon(Icons.close),
+                      ),
+                    ],
                   ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: imageController,
+                    decoration: InputDecoration(
+                      labelText: 'URL da Imagem Tarefa',
+                    ),
                   ),
-                  IconButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();  
-                    },
-                    icon: Icon(Icons.close),
+                  SizedBox(height: 10),
+                  TextField(
+                    controller: descricaoController,
+                    decoration: InputDecoration(
+                      labelText: 'Descrição da Tarefa',
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          //ação do botão salvar
+                          _adicionarTarefas(
+                            imageController.text,
+                            descricaoController.text,
+                            context,
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Tarefa cadastrada com Sucesso'),
+                              duration: Duration(seconds: 3),
+                            ),
+                          );
+                          Navigator.of(context).pop();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black38,
+                        ),
+                        child: Text(
+                          'Cadastar',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.amber,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          //ação do botão
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black38,
+                        ),
+                        child: Text(
+                          'Cancelar',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.amber,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-              SizedBox(height: 10),
-              TextField(
-                controller: imageController,
-                decoration: InputDecoration(
-                  labelText: 'URL da Imagem da Tarefa'
-                ),
-              ),
-              TextField(
-                controller: descricaoController,
-                decoration: InputDecoration(
-                  labelText: 'Descrição da Tarefa',
-                ),
-              ),
-              SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      _adicionarTarefas(
-                        imageController.text,
-                        descricaoController.text,
-                        context,
-                        );
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text(
-                              'Cadastrada com sucesso!'
-                            ),
-                              duration: Duration( seconds: 3),
-                            ),
-                        );
-                      Navigator.of(context).pop();
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.lightGreen),
-                    child: Text('Cadastrar',
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red),
-                    child: Text('Cancelar',
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          )
-        )
-        ),
+            ),
+          ),
         );
       },
-      );
+    );
   }
-
-
+ 
+  //Criando Modal de Editar
+  //Criando um modal
+  //metodo void Editar
+  void _showFormEdit(BuildContext context) {
+    //criando as variaveis de controller
+    final TextEditingController imageController = TextEditingController();
+    final TextEditingController descricaoController = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SingleChildScrollView(
+            //Retangulo vai se adaptar ao conteudo aos componentes
+            child: Container(
+              //Criando o container
+              //margem interna
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Editar Atividade",
+                        style: TextStyle(
+                          fontFamily: "Verdana",
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          //ação do icone botão fechar
+                          Navigator.of(context).pop();
+                        },
+                        icon: Icon(Icons.close),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: imageController,
+                    decoration: InputDecoration(
+                      labelText: 'URL da Imagem Tarefa',
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    controller: descricaoController,
+                    decoration: InputDecoration(
+                      labelText: 'Descrição da Tarefa',
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          //ação do botão salvar
+                        },
+                        child: Text(
+                          'Editar',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.amber,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        child: Text('Cancelar'),
+                        onPressed: () {
+                          //ação do botão
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black38,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Tarefas"),
-        backgroundColor: Colors.pink,
+        backgroundColor: Colors.amber,
       ),
-      body: ListView(
-        children: [
-          Tarefas(
-            "https://cdn2.hubspot.net/hubfs/475261/7._Como_gerenciar_melhor_seu_tempo_para_executar_todas_as_tarefas_do_dia.jpg",
-          ),
-          Tarefas(
-            "https://www.psicologosberrini.com.br/wp-content/uploads/lista-de-tarefas-para-mudar-sua-vida.jpeg",
-          ),
-          Tarefas(
-            "https://seculoxximinas.com.br/fgv/wp-content/uploads/2018/12/Webp.net-compress-image-20.jpg",
-                      ),
-        ],
+      body: ListView.builder(
+        itemCount: _tarefas.length,
+        itemBuilder: (context, index) {
+          return Tarefas(
+            _tarefas[index]['url']!,
+            _tarefas[index]['descricao']!,
+            () => _showFormEdit(context),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          //ação do botão
           _showForm(context);
         },
         child: Icon(Icons.add),
-        ),
+      ),
     );
   }
 }
@@ -155,102 +259,18 @@ class _MyHomePageState extends State<MyHomePage> {
 //Criando a classe Tarefas
 class Tarefas extends StatelessWidget {
   final String imagem_url;
+  final String descricao;
+  final VoidCallback onEdit;
  
-  void _showEditar (BuildContext context) {
-    final TextEditingController simboloController = TextEditingController();
-    final TextEditingController descreverController = TextEditingController();
-    showDialog(
-      context: context, 
-      builder: (BuildContext context){
-        return Dialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-        ),
-        child:SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Editar Atividade",
-                  style: TextStyle(
-                    fontFamily:"Verdana",
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();  
-                    },
-                    icon: Icon(Icons.close),
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              TextField(
-                controller: simboloController,
-                decoration: InputDecoration(
-                  labelText: 'URL da Imagem da Tarefa'
-                ),
-              ),
-              TextField(
-                controller: descreverController,
-                decoration: InputDecoration(
-                  labelText: 'Descrição da Tarefa',
-                ),
-              ),
-              SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.yellow),
-                    child: Text('Editar',
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red),
-                    child: Text('Cancelar',
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          )
-        )
-        ),
-        );
-      },
-      );
-  }
-
-  const Tarefas(this.imagem_url, {super.key});
-
+  const Tarefas(this.imagem_url, this.descricao, this.onEdit, {super.key});
+ 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(5.0),
       child: Stack(
         children: [
-          Container(color: Colors.purpleAccent, height: 140),
+          Container(color: Color.fromARGB(255, 115, 211, 214), height: 140),
           Container(
             color: Colors.white,
             height: 100,
@@ -267,18 +287,13 @@ class Tarefas extends StatelessWidget {
                   ),
                 ),
                 Expanded(
-                  child: Text(
-                    "Tarefa - Descrição",
-                    style: TextStyle(fontSize: 20),
-                  ),
+                  child: Text(descricao, style: TextStyle(fontSize: 20)),
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(8.0), 
+                  padding: const EdgeInsets.all(8.0),
                   child: ElevatedButton(
-                    onPressed: () {
-                      _showEditar(context);
-                    },
-                    child: Icon(Icons.edit), // edição dos botôes de lápis
+                    onPressed: onEdit,
+                    child: Icon(Icons.edit),
                   ),
                 ),
               ],
