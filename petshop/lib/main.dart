b52f57875edb1ecf57e3f71d67dfb010d6bca4ce
Beatriz <biabatista1219@gmail.com>
2025-09-12 08:49:03 -0300

    to tentando arrumar

--- petshop/lib/main.dart
+++ petshop/lib/main.dart
@@ -1,60 +1,20 @@
 import 'package:flutter/material.dart';
 import 'pet_pages.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const PetShopApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PetShopApp extends StatelessWidget {
+  const PetShopApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PetShop CRUD',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.blue,
-        ).copyWith(secondary: Colors.orangeAccent),
-      ),
-      home: MyHomePage(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('PetShop CRUD')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AddPetPage()),
-                );
-              },
-              child: const Text('Add Pet'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ViewPetsPage()),
-                );
-              },
-              child: const Text('View Pets'),
-            ),
-          ],
-        ),
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
     );
   }
 }
--- petshop/test/widget_test.dart
+++ petshop/test/widget_test.dart
@@ -6,7 +6,7 @@ import 'package:petshop/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-  await tester.pumpWidget(MyApp());
+  await tester.pumpWidget(PetShopApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
