f4c13f5d02158c3b505ad839c5da63bc976d32c5
Beatriz <biabatista1219@gmail.com>
2025-10-03 09:16:53 -0300

    ihuuuuuuuuuuuu

---
+++ app_localizacao_historico/lib/localizacao/geo.dart
@@ -0,0 +1,19 @@
+import 'package:geolocator/geolocator.dart';
+
+Future<Position> getLocalizacaoAtual () async{
+  
+  bool serviceEnable = await Geolocator.isLocationServiceEnabled();
+  if(!serviceEnable){
+    return Future.error("Serviço de localização desabilitado");
+
+  }
+
+  LocationPermission permissao = await Geolocator.checkPermission();
+  if(permissao == LocationPermission.denied){
+    permissao = await Geolocator.requestPermission();
+    if(permissao == LocationPermission.denied){
+      return Future.error("Permissão de localização negada");
+    }
+    
+  }
+}
